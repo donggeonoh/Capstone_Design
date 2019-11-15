b0a28a9c5712841b690fa157a9fbefae7ebe6f66
dptmf7705 <sdc01194@gmail.com>
2019-11-16 03:04:46 +0900

    음식 리스트

--- app/src/main/java/com/donggeon/honmaker/ui/food/FoodViewModel.java
+++ app/src/main/java/com/donggeon/honmaker/ui/food/FoodViewModel.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.MutableLiveData;
 import com.donggeon.honmaker.ui.BaseViewModel;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @SuppressLint("CheckResult")
@@ -17,13 +18,38 @@ public class FoodViewModel extends BaseViewModel {
     private final MutableLiveData<List<Food>> foodList = new MutableLiveData<>();
 
     public FoodViewModel() {
-        List<Food> foodList = new ArrayList<>();
-        for (int i = 0; i < 10; i++) {
-            foodList.add(new Food(
-                    "양파전",
-                    "http://file.okdab.com/UserFiles/searching/recipe/119300.jpg",
-                    "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=421&own="));
-        }
+        List<Food> foodList = new ArrayList<>(Arrays.asList(
+                new Food("양파전",
+                        "http://file.okdab.com/UserFiles/searching/recipe/119300.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=421&own="),
+                new Food("소고기무국",
+                        "http://file.okdab.com/UserFiles/searching/recipe/049400.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=202&own="),
+                new Food("수정과",
+                        "http://file.okdab.com/UserFiles/searching/recipe/039800.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=171&own="),
+                new Food("미역국",
+                        "http://file.okdab.com/UserFiles/searching/recipe/004700.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=21&own="),
+                new Food("양파전",
+                        "http://file.okdab.com/UserFiles/searching/recipe/119300.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=421&own="),
+                new Food("소고기무국",
+                        "http://file.okdab.com/UserFiles/searching/recipe/049400.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=202&own="),
+                new Food("수정과",
+                        "http://file.okdab.com/UserFiles/searching/recipe/039800.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=171&own="),
+                new Food("미역국",
+                        "http://file.okdab.com/UserFiles/searching/recipe/004700.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=21&own="),
+                new Food("양파전",
+                        "http://file.okdab.com/UserFiles/searching/recipe/119300.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=421&own="),
+                new Food("양파전",
+                        "http://file.okdab.com/UserFiles/searching/recipe/119300.jpg",
+                        "http://www.horangi.kr/foodinfo/viewRecipe.asp?rid=421&own=")));
+
         this.foodList.setValue(foodList);
     }
 
