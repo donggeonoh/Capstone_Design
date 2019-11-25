9a5cf2eed5d58e8bff2fcda457d08c0cbaafdd79
OhDonggeon <cafa3@naver.com>
2019-11-25 20:37:36 +0900

    modified RetrofitAPI

--- app/src/main/java/com/donggeon/honmaker/extension/Retrofit/RetrofitClient.java
+++ app/src/main/java/com/donggeon/honmaker/extension/Retrofit/RetrofitClient.java
@@ -5,10 +5,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
     
-    //http://52.79.234.234:80/
-    
     public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8081/")
+            .baseUrl("http://52.79.234.234:80/")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
 }
